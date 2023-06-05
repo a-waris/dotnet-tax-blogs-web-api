a23c2a7ea3f536ea6ef32d922a0507bb70a21081
CodingDock <coding.dockk@gmail.com>
2023-06-05 19:40:06 +0000

    minor update

--- src/Taxbox.Infrastructure/Context/ApplicationDbContext.cs
+++ src/Taxbox.Infrastructure/Context/ApplicationDbContext.cs
@@ -3,20 +3,13 @@ using Taxbox.Domain.Entities;
 using Taxbox.Infrastructure.Configuration;
 using EntityFramework.Exceptions.SqlServer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace Taxbox.Infrastructure.Context;
 
 public class ApplicationDbContext : DbContext, IContext
 {
-    private readonly IConfiguration? _configuration;
+    public ApplicationDbContext() { }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration)
-        : base(options)
-    {
-        _configuration = configuration;
-    }
 
     public DbSet<Hero> Heroes { get; set; } = null!;
 
@@ -26,10 +19,6 @@ public class ApplicationDbContext : DbContext, IContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
-        if (!optionsBuilder.IsConfigured)
-        {
-            optionsBuilder.UseSqlServer(_configuration?.GetConnectionString("DefaultConnection"));
-        }
 
         optionsBuilder.UseExceptionProcessor();
     }
