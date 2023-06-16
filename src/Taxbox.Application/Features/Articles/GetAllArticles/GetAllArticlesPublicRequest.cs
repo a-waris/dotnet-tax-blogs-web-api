ec7438bdf7c21b7c3372e8a066119eb3ffe22a0f
A.Waris <andrew.waris@convergetp.com>
2023-06-16 13:09:28 +0500

    fix public list

--- src/Taxbox.Application/Features/Articles/GetAllArticles/GetAllArticlesPublicRequest.cs
+++ src/Taxbox.Application/Features/Articles/GetAllArticles/GetAllArticlesPublicRequest.cs
@@ -26,6 +26,6 @@ public record GetAllArticlesPublicRequest : PaginatedRequest, IRequest<Paginated
     public override string ToString()
     {
         return
-            $"{Title} {Metadata} {Content} {AuthorIds} {CreatedAt} {UpdatedAt} {Tags} {IsPublic} {IsPublished} {SourceFields} {FreeTextSearch} {IsDraft} {PublishedAt}";
+            $"{Title} {Metadata} {Content} {AuthorIds} {CreatedAt} {UpdatedAt} {Tags} {IsPublished} {SourceFields} {FreeTextSearch} {IsDraft} {PublishedAt}";
     }
 }
