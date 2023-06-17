24abc3edd11619bc9bdcc2adb1e0ef15e69bf975
A.Waris <andrew.waris@convergetp.com>
2023-06-18 00:56:52 +0500

    cleanup

--- src/Taxbox.Application/Features/UserSubscriptions/CreateUserSubscription/CreateUserSubscriptionHandler.cs
+++ src/Taxbox.Application/Features/UserSubscriptions/CreateUserSubscription/CreateUserSubscriptionHandler.cs
@@ -33,117 +33,104 @@ public class
     {
         var created = request.Adapt<UserSubscription>();
 
-        // get subscription plan
         var sub = await _context.Subscriptions.FirstOrDefaultAsync(x => x.Id == request.SubscriptionId,
             cancellationToken);
+        if (sub == null)
+            return Result.NotFound("Subscription not found");
 
-        if (sub == null) return Result.NotFound("Subscription not found");
-
-
-        // if trial start date is set then set trial end date based on the subscription
         if (request.TrialStartDate is not null)
         {
-            // save as UTC
-            created.TrialStartDate = request.TrialStartDate.Value.ToUniversalTime();
-            created.TrialEndDate = sub.ValidityPeriodType.ToLower() switch
-            {
-                "days" => created.TrialStartDate.Value.AddDays(sub.ValidityPeriod),
-                "months" => created.TrialStartDate.Value.AddMonths(sub.ValidityPeriod),
-                _ => created.TrialEndDate
-            };
-            // Lets not set NextBillingDate for trial subscriptions
+            SetTrialDates(created, request.TrialStartDate.Value, sub.ValidityPeriodType, sub.ValidityPeriod);
         }
-        // else if subscription start date is set then set subscription end date based on the subscription plan
         else if (request.SubscriptionStartDate is not null)
         {
-            // save as UTC
-            created.SubscriptionStartDate = request.SubscriptionStartDate.Value.ToUniversalTime();
-            created.SubscriptionEndDate = sub.ValidityPeriodType.ToLower() switch
-            {
-                "days" => created.SubscriptionStartDate.Value.AddDays(sub.ValidityPeriod),
-                "months" => created.SubscriptionStartDate.Value.AddMonths(sub.ValidityPeriod),
-                _ => created.SubscriptionEndDate
-            };
-            // set NextBillingDate to SubscriptionEndDate + 1 day
-            created.NextBillingDate = created.SubscriptionEndDate?.AddDays(1);
-
-            // TODO: handle coupon code
-            // if coupon code is set
-            if (created.CouponCode != default)
-            {
-            }
-
-            // TODO: handle discount
-            // if discount is set 
-            if (request.DiscountAmount != default)
-            {
-            }
-
-            // get the user 
+            SetSubscriptionDates(created, request.SubscriptionStartDate.Value, sub.ValidityPeriodType,
+                sub.ValidityPeriod);
+            await HandleCouponCode(request, created);
+            await HandleDiscount(request);
             var user = await _context.Users.FindAsync(request.UserId);
-
-            // if user is not found then return error
-            if (user == null) return Result.NotFound("User not found");
-
-
+            if (user == null)
+                return Result.NotFound("User not found");
             await HandlePaymentFlow(request, cancellationToken, user, created, sub);
         }
 
         _context.UserSubscriptions.Add(created);
-
         await _context.SaveChangesAsync(cancellationToken);
         return created.Adapt<GetUserSubscriptionResponse>();
     }
 
-    private async Task HandlePaymentFlow(CreateUserSubscriptionRequest request,
-        CancellationToken cancellationToken,
+    private void SetTrialDates(UserSubscription created, DateTime trialStartDate, string validityPeriodType,
+        int validityPeriod)
+    {
+        created.TrialStartDate = trialStartDate.ToUniversalTime();
+        created.TrialEndDate = validityPeriodType.ToLower() switch
+        {
+            "days" => created.TrialStartDate.Value.AddDays(validityPeriod),
+            "months" => created.TrialStartDate.Value.AddMonths(validityPeriod),
+            _ => created.TrialEndDate
+        };
+    }
+
+    private void SetSubscriptionDates(UserSubscription created, DateTime subscriptionStartDate,
+        string validityPeriodType, int validityPeriod)
+    {
+        created.SubscriptionStartDate = subscriptionStartDate.ToUniversalTime();
+        created.SubscriptionEndDate = validityPeriodType.ToLower() switch
+        {
+            "days" => created.SubscriptionStartDate.Value.AddDays(validityPeriod),
+            "months" => created.SubscriptionStartDate.Value.AddMonths(validityPeriod),
+            _ => created.SubscriptionEndDate
+        };
+        created.NextBillingDate = created.SubscriptionEndDate?.AddDays(1);
+    }
+
+    private async Task HandleCouponCode(CreateUserSubscriptionRequest request, UserSubscription created)
+    {
+        if (created.CouponCode != default)
+        {
+            // TODO: Handle coupon code
+        }
+        
+    }
+
+    private async Task HandleDiscount(CreateUserSubscriptionRequest request)
+    {
+        if (request.DiscountAmount != default)
+        {
+            // TODO: Handle discount
+        }
+    }
+
+    private async Task HandlePaymentFlow(CreateUserSubscriptionRequest request, CancellationToken cancellationToken,
         User user, UserSubscription created, Subscription sub)
     {
         try
         {
-            // handle stripe subscription
             if (request.CardDetails == null)
-            {
-                return;
-            }
+                throw new Exception("Card details not provided");
 
-            var createCustomerResource = new CreateCustomerResource(
-                user.Email, $"{user.FirstName} {user.LastName}", request.CardDetails
-            );
-
-            var customer = await _stripeService.CreateCustomer(
-                createCustomerResource, cancellationToken
-            );
-
-            // set stripe customer id
+            var createCustomerResource =
+                new CreateCustomerResource(user.Email, $"{user.FirstName} {user.LastName}", request.CardDetails);
+            var customer = await _stripeService.CreateCustomer(createCustomerResource, cancellationToken);
+            
+            // set customer id
             created.CustomerId = customer.CustomerId;
 
             if (_configuration.GetSection("StripeOptions")["Mode"] == "live")
             {
-                // create charge
-                var charge = await _stripeService.CreateCharge(
-                    new CreateChargeResource(
-                        Currency: sub.Currency,
-                        // parse into long and multiply by 100 to convert to cents
-                        Amount: (long)(sub.Amount * 100),
-                        Description: "Subscription charge for " + sub.Name,
-                        CustomerId: customer.CustomerId,
-                        ReceiptEmail: user.Email
-                    ), cancellationToken
-                );
-            }
-
-
-            var intent = await _stripeService.CreatePaymentIntent(
-                new CreatePaymentIntentResource(
+                var charge = await _stripeService.CreateCharge(new CreateChargeResource(
                     Currency: sub.Currency,
-                    // parse into long and multiply by 100 to convert to cents
                     Amount: (long)(sub.Amount * 100),
                     Description: "Subscription charge for " + sub.Name,
-                    ReceiptEmail: user.Email
-                ),
-                cancellationToken
-            );
+                    CustomerId: customer.CustomerId,
+                    ReceiptEmail: user.Email), cancellationToken);
+            }
+
+            var intent = await _stripeService.CreatePaymentIntent(new CreatePaymentIntentResource(
+                Currency: sub.Currency,
+                Amount: (long)(sub.Amount * 100),
+                Description: "Subscription charge for " + sub.Name,
+                ReceiptEmail: user.Email), cancellationToken);
         }
         catch (Exception e)
         {
