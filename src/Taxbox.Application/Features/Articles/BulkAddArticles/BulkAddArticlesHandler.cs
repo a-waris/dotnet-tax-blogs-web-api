72ae6c1e4d0646aecc0ff52be5b38dd555a4f691
A.Waris <andrew.waris@convergetp.com>
2023-06-18 14:17:49 +0500

    Fix parallel processing

--- src/Taxbox.Application/Features/Articles/BulkAddArticles/BulkAddArticlesHandler.cs
+++ src/Taxbox.Application/Features/Articles/BulkAddArticles/BulkAddArticlesHandler.cs
@@ -13,6 +13,7 @@ using System.Threading.Tasks;
 using Taxbox.Application.Extensions;
 using Taxbox.Domain.ElasticSearch.Interfaces;
 using Taxbox.Domain.Entities;
+using static System.Int32;
 
 namespace Taxbox.Application.Features.Articles.BulkAddArticles;
 
@@ -44,105 +45,70 @@ public class BulkAddArticlesHandler : IRequestHandler<BulkAddArticlesRequest, Re
     {
         try
         {
-            // check if file is excel file 
             var fileExtension = Path.GetExtension(requestFile.FileName);
             if (fileExtension != ".xlsx" && fileExtension != ".xls")
             {
                 throw new Exception("Invalid file type");
             }
 
-
-            // fetch taxbox author by name "Taxbox" from elastic search
             var taxboxAuthorResp = await _esServiceAuthor.Index("authors").Query(
                 new SearchRequestDescriptor<Author>()
                     .Query(q => q.RawJson("{\"term\": {\"name.raw\": {\"value\": \"Taxbox\"}}}"))
                     .Size(1));
 
             var author = taxboxAuthorResp?.Documents.FirstOrDefault();
+            if (author == null)
+            {
+                throw new Exception("Taxbox author not found");
+            }
 
             ConcurrentBag<Article> articles = new();
-            Dictionary<string, Article> addedArticles = new Dictionary<string, Article>();
+            Dictionary<string, Article> addedArticles = new();
 
             using var package = new ExcelPackage(requestFile.OpenReadStream());
             ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
             worksheet.TrimLastEmptyRows();
 
             int rowCount = worksheet.Dimension.Rows;
-
-            bulkAddArticlesResponse.TotalRows = rowCount - 1; // -1 for header row
-            int fromInclusive =  rowCount > 2 ? 2 : 1;
-            object lockObject = new object();
-            Parallel.ForEach(Partitioner.Create(fromInclusive, rowCount), range =>
+            bulkAddArticlesResponse.TotalRows = rowCount - 1;
+            switch (rowCount)
             {
-                for (int row = range.Item1; row <= range.Item2; row++) // Assuming the data starts from row 2
-                {
-                    if (string.IsNullOrEmpty(worksheet.Cells[row, 1].Value.ToString())
-                        || string.IsNullOrEmpty(worksheet.Cells[row, 2].Value.ToString())
-                       )
+                case < 2:
+                    throw new Exception("No data found in excel");
+                case > 1000:
+                    throw new Exception("Maximum 1000 rows are allowed");
+                case 2:
                     {
-                        bulkAddArticlesResponse.InvalidRows += 1;
-                        continue;
-                    }
-
-                    var article = new Article
-                    {
-                        Title = worksheet.Cells[row, 1].Value.ToString()!,
-                        Content = worksheet.Cells[row, 2].Value?.ToString(),
-                        HtmlContent = worksheet.Cells[row, 3].Value?.ToString(),
-                        Metadata = new Metadata
+                        var article = ProcessRow(worksheet, 2, author, bulkAddArticlesResponse);
+                        if (article != null)
                         {
-                            Category = worksheet.Cells[row, 4].Value?.ToString(),
-                            Language = worksheet.Cells[row, 5].Value?.ToString(),
-                            Views = Convert.ToInt32(worksheet.Cells[row, 6].Value)
-                        },
-                        AuthorIds =
-                            new List<string> { worksheet.Cells[row, 7].Value?.ToString() ?? author?.Id.ToString()! },
-                        PublishedAt = worksheet.Cells[row, 8].Value?.ToString() != null
-                            ? Convert.ToDateTime(worksheet.Cells[row, 8].Value)
-                            : DateTime.Now.Date,
-                        Tags = worksheet.Cells[row, 9].Value?.ToString()?.Split(',').ToList(),
-                        IsDraft =
-                            worksheet.Cells[row, 10].Value?.ToString()?.ToLower() == "true" ||
-                            worksheet.Cells[row, 10].Value?.ToString() == "1",
-                        IsPublic =
-                            worksheet.Cells[row, 11].Value?.ToString()?.ToLower() == "true" ||
-                            worksheet.Cells[row, 11].Value?.ToString() == "1",
-                        IsPublished = worksheet.Cells[row, 12].Value?.ToString()?.ToLower() == "true" ||
-                                      worksheet.Cells[row, 12].Value?.ToString() == "1",
-                        CoverImage = worksheet.Cells[row, 13].Value?.ToString(),
-                        ThumbnailImage = worksheet.Cells[row, 14].Value?.ToString(),
-                        // Do not add attachments for now
-                        // Attachments = new List<ArticleAttachment>
-                        // {
-                        //     new()
-                        //     {
-                        //         File = worksheet.Cells[row, 14].Value?.ToString() ?? "",
-                        //         Type = worksheet.Cells[row, 15].Value?.ToString() ?? ""
-                        //     }
-                        // },
-                        CreatedAt = DateTime.Now.Date,
-                        UpdatedAt = DateTime.Now.Date,
-                    };
-
-                    bool isAdded = false;
-
-                    lock (lockObject)
-                    {
-                        if (!addedArticles.ContainsKey(article.Title))
-                        {
-                            addedArticles.Add(article.Title, article);
-                            isAdded = true;
+                            articles.Add(article);
                         }
+
+                        return articles.ToList();
+                    }
+            }
+
+            Parallel.For(2, rowCount + 1, row =>
+            {
+                var article = ProcessRow(worksheet, row, author, bulkAddArticlesResponse);
+                if (article == null)
+                {
+                    return;
+                }
+
+                lock (addedArticles)
+                {
+                    if (addedArticles.ContainsKey(article.Title))
+                    {
+                        return;
                     }
 
-                    if (isAdded)
+                    addedArticles.Add(article.Title, article);
+                    articles.Add(article);
+                    lock (bulkAddArticlesResponse)
                     {
-                        articles.Add(article);
-                        bulkAddArticlesResponse.SuccessfulRows += 1;
-                    }
-                    else
-                    {
-                        // bulkAddArticlesResponse.DuplicateRows += 1;
+                        bulkAddArticlesResponse.SuccessfulRows++;
                     }
                 }
             });
@@ -154,5 +120,55 @@ public class BulkAddArticlesHandler : IRequestHandler<BulkAddArticlesRequest, Re
             Console.WriteLine(e);
             throw;
         }
+    }
+
+    private Article? ProcessRow(ExcelWorksheet worksheet, int rowIndex, Author author,
+        BulkAddArticlesResponse bulkAddArticlesResponse)
+    {
+        if (string.IsNullOrEmpty(worksheet.Cells[rowIndex, 1]?.Value.ToString())
+            || string.IsNullOrEmpty(worksheet.Cells[rowIndex, 2]?.Value.ToString()))
+        {
+            lock (bulkAddArticlesResponse)
+            {
+                bulkAddArticlesResponse.InvalidRows++;
+            }
+
+            return null;
+        }
+
+        if (!TryParse(worksheet.Cells[rowIndex, 6].Value?.ToString(), out int views))
+        {
+            views = 0;
+        }
+
+        var article = new Article
+        {
+            Title = worksheet.Cells[rowIndex, 1].Value.ToString()!,
+            Content = worksheet.Cells[rowIndex, 2].Value?.ToString(),
+            HtmlContent = worksheet.Cells[rowIndex, 3].Value?.ToString(),
+            Metadata = new Metadata
+            {
+                Category = worksheet.Cells[rowIndex, 4].Value?.ToString(),
+                Language = worksheet.Cells[rowIndex, 5].Value?.ToString(),
+                Views = views
+            },
+            AuthorIds = new List<string> { worksheet.Cells[rowIndex, 7].Value?.ToString() ?? author.Id.ToString() },
+            PublishedAt = worksheet.Cells[rowIndex, 8].Value?.ToString() != null
+                ? Convert.ToDateTime(worksheet.Cells[rowIndex, 8].Value)
+                : DateTime.Now.Date,
+            Tags = worksheet.Cells[rowIndex, 9].Value?.ToString()?.Split(',').ToList(),
+            IsDraft = worksheet.Cells[rowIndex, 10].Value?.ToString()?.ToLower() == "true"
+                      || worksheet.Cells[rowIndex, 10].Value?.ToString() == "1",
+            IsPublic = worksheet.Cells[rowIndex, 11].Value?.ToString()?.ToLower() == "true"
+                       || worksheet.Cells[rowIndex, 11].Value?.ToString() == "1",
+            IsPublished = worksheet.Cells[rowIndex, 12].Value?.ToString()?.ToLower() == "true"
+                          || worksheet.Cells[rowIndex, 12].Value?.ToString() == "1",
+            CoverImage = worksheet.Cells[rowIndex, 13].Value?.ToString(),
+            ThumbnailImage = worksheet.Cells[rowIndex, 14].Value?.ToString(),
+            CreatedAt = DateTime.Now.Date,
+            UpdatedAt = DateTime.Now.Date
+        };
+
+        return article;
     }
 }
