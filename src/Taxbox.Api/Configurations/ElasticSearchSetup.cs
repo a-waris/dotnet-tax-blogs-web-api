357b08331bd3e25aee32f7f748a71bc3fb7fb48b
A.Waris <andrew.waris@convergetp.com>
2023-06-18 14:31:03 +0500

    Fix appsettings for es setup

--- src/Taxbox.Api/Configurations/ElasticSearchSetup.cs
+++ src/Taxbox.Api/Configurations/ElasticSearchSetup.cs
@@ -3,10 +3,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Elastic.Clients.Elasticsearch;
 using Elastic.Transport;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
 using System;
 using Taxbox.Application.Common;
-using Taxbox.Domain.Entities;
 using Taxbox.Infrastructure.ElasticSearch;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
@@ -29,94 +27,93 @@ public static class ElasticSearchSetup
             throw new Exception("ElasticSearchConfiguration is not configured");
         }
 
-        var defaultIndex = appSettings.DefaultIndex;
+        var index = appSettings.ArticlesIndex;
         var debugMode = appSettings.EnableDebugMode;
-        var settings = EsSettings(appSettings, builder);
+        var settings = EsSettings(appSettings);
         if (debugMode)
         {
             settings.EnableDebugMode();
         }
 
-        // AddDefaultMappings(settings, defaultIndex);
+        // AddDefaultMappings(settings, index);
 
-        var client = new ElasticClientContainer(settings, defaultIndex);
+        var client = new ElasticClientContainer(settings, index);
         services.AddSingleton<IElasticsearchClientSettings>(settings);
         services.AddSingleton<IElasticClientContainer>(client);
         services.AddScoped(typeof(IElasticSearchService<>), typeof(ElasticSearchService<>));
 
-        // CreateIndex(client, defaultIndex);
+        // CreateIndex(client, index);
 
         return services;
     }
 
-    private static ElasticsearchClientSettings EsSettings(ElasticSearchConfiguration esConfig,
-        WebApplicationBuilder builder)
+    private static ElasticsearchClientSettings EsSettings(ElasticSearchConfiguration esConfig)
     {
         var auth = new BasicAuthentication(esConfig.User, esConfig.Password);
-        if (builder.Environment.IsProduction())
+        if (esConfig.UseLocalEs)
         {
-            return new ElasticsearchClientSettings(esConfig.CloudId, auth)
-                .DefaultIndex(esConfig.DefaultIndex);
+            var nodes = new Uri[] { new(esConfig.Url) };
+            var pool = new StaticNodePool(nodes);
+            return new ElasticsearchClientSettings(pool)
+                .CertificateFingerprint(GetSha2Thumbprint(new X509Certificate2(esConfig.ClientCertificatePath)))
+                .Authentication(auth)
+                .DefaultIndex(esConfig.ArticlesIndex);
         }
 
-        var nodes = new Uri[] { new(esConfig.Url) };
-        var pool = new StaticNodePool(nodes);
-        return new ElasticsearchClientSettings(pool)
-            .CertificateFingerprint(GetSha2Thumbprint(new X509Certificate2(esConfig.ClientCertificatePath)))
-            .Authentication(auth)
-            .DefaultIndex(esConfig.DefaultIndex);
+        return new ElasticsearchClientSettings(esConfig.CloudId, auth)
+            .DefaultIndex(esConfig.ArticlesIndex);
     }
 
-    private static void AddDefaultMappings(ElasticsearchClientSettings settings, string defaultIndex)
-    {
-        settings
-            .DefaultMappingFor<Article>
-            (i => i
-                .IndexName(defaultIndex)
-                .IdProperty(a => a.Id)
-            )
-            .PrettyJson();
-    }
+    // private static void AddDefaultMappings(ElasticsearchClientSettings settings, string index)
+    // {
+    //     settings
+    //         .DefaultMappingFor<Article>
+    //         (i => i
+    //             .IndexName(index)
+    //             .IdProperty(a => a.Id)
+    //         )
+    //         .PrettyJson();
+    // }
 
-    private static async void CreateIndex(IElasticClientContainer clientContainer, string indexName)
-    {
-        var client = clientContainer.GetElasticClient();
-        var indexExists = await client.Indices.ExistsAsync(indexName);
-        if (!indexExists.IsValidResponse)
-        {
-            throw new Exception(indexExists.DebugInformation);
-        }
-
-        if (indexExists.Exists)
-        {
-            return;
-        }
-
-        var createIndexResponse = await client.Indices.CreateAsync(indexName,
-            index => index.Mappings(descriptor =>
-                descriptor.Properties<Article>(p => p
-                    .Text(t => t.Title)
-                    .Object(o => o.Metadata!)
-                    .Text(t => t.Content!)
-                    .Keyword(k => k.AuthorIds!)
-                    .Date(d => d.CreatedAt!)
-                    .Date(d => d.UpdatedAt!)
-                    .Keyword(k => k.Tags!)
-                )
-            ).Settings(descriptor =>
-                descriptor.Analysis(analysis =>
-                    analysis
-                        .Analyzers(analyzers => analyzers
-                            .Standard("standard")
-                        )
-                ))
-        );
-
-        if (!createIndexResponse.IsValidResponse)
-        {
-            throw new Exception(createIndexResponse.DebugInformation);
-        }
-    }
+    // private static async void CreateIndex(IElasticClientContainer clientContainer, string indexName)
+    // {
+    //     var client = clientContainer.GetElasticClient();
+    //     var indexExists = await client.Indices.ExistsAsync(indexName);
+    //     if (!indexExists.IsValidResponse)
+    //     {
+    //         throw new Exception(indexExists.DebugInformation);
+    //     }
+    //
+    //     if (indexExists.Exists)
+    //     {
+    //         return;
+    //     }
+    //
+    //     var createIndexResponse = await client.Indices.CreateAsync(indexName,
+    //         index => index.Mappings(descriptor =>
+    //             descriptor.Properties<Article>(p => p
+    //                 .Text(t => t.Title)
+    //                 .Object(o => o.Metadata!)
+    //                 .Text(t => t.Content!)
+    //                 .Keyword(k => k.AuthorIds!)
+    //                 .Date(d => d.CreatedAt!)
+    //                 .Date(d => d.UpdatedAt!)
+    //                 .Keyword(k => k.Tags!)
+    //             )
+    //         ).Settings(descriptor =>
+    //             descriptor.Analysis(analysis =>
+    //                 analysis
+    //                     .Analyzers(analyzers => analyzers
+    //                         .Standard("standard")
+    //                     )
+    //             ))
+    //     );
+    //
+    //     if (!createIndexResponse.IsValidResponse)
+    //     {
+    //         throw new Exception(createIndexResponse.DebugInformation);
+    //     }
+    // }
 
     private static string GetSha2Thumbprint(X509Certificate2 cert)
     {
